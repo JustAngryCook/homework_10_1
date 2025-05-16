d3b29db90eb9560bcb11e1f602e14a7ac4a8fdd9
Alexandr Pashkovsky <alexpashkovsky1989@gmail.com>
2025-05-17 02:38:50 +0400

    В модуль processing добавлена функция сортировки опреций по дате

--- src/processing.py
+++ src/processing.py
@@ -5,3 +5,11 @@ def filter_by_state(list_of_dictionaries: list[dict], state: str = "EXECUTED") -
         if d.get("state") == state:
             new_dict.append(d)
     return new_dict
+
+
+def sort_by_date(list_of_dictionaries: list[dict], sorting: bool = True) -> list[dict]:
+    """функция принимает список словарей и сортирует по дате операции"""
+    if sorting:
+        return sorted(list_of_dictionaries, key=lambda d: d["date"], reverse=True)
+    else:
+        return sorted(list_of_dictionaries, key=lambda d: d["date"])
